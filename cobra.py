594433e77b55c1220dc2e70e831b1d5842adbdb3
Felipe <felipecarneiro@duck.com>
2022-05-04 15:38:33 -0300

    módulo 1

---
+++ cobra.py
@@ -0,0 +1,10 @@
+onomatopéia =["Sssss...sss...","Roawh!","Miau...","Roink!"]
+def imprima(x):
+    cont = 0
+    while cont < len(x):
+        print(x[cont], sep="-", end=" ")
+        cont += 1
+
+imprima(onomatopéia)
+
+
